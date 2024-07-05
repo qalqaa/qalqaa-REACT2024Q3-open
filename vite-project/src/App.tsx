c8fb27133912b8fa488689abea9afc70737e2b81
qalqa <andreybas04@gmail.com>
2024-07-05 20:46:39 +0300

    refactor: remade App.tsx like class component

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,40 +1,53 @@
-import { useCallback, useEffect, useState } from "react";
-import "./App.css";
+import { Component } from "react";
 import SearchInput from "./components/SearchInput";
 import SearchResults from "./components/SearchResults";
 import axios from "axios";
+import "./App.css";
 
-interface IResults {
+interface IAppState {
+  results: IResult[];
+}
+
+interface IResult {
   name: string;
   description: string;
 }
 
-function App() {
-  const [results, setResults] = useState<IResults[]>([]);
+class App extends Component<IResult, IAppState> {
+  constructor(props: IResult) {
+    super(props);
+    this.state = {
+      results: [],
+    };
+  }
 
-  const getData = useCallback(async (searchTerm: string | null) => {
+  componentDidMount() {
+    this.getData(localStorage.getItem("searchTerm") || "");
+  }
+
+  getData = async (searchTerm: string) => {
     const url = searchTerm
       ? `https://swapi.dev/api/people/?search=${searchTerm}`
       : `https://swapi.dev/api/people/`;
+
     try {
       const res = await axios.get(url);
-      setResults(res.data.results);
+      this.setState({ results: res.data.results });
     } catch (error) {
       console.error("Error", error);
     }
-  }, []);
+  };
 
-  useEffect(() => {
-    const savedTerm = localStorage.getItem("searchTerm") || "";
-    getData(savedTerm);
-  }, [getData]);
+  render() {
+    const { results } = this.state;
 
-  return (
-    <>
-      <SearchInput onSearch={getData} />
-      <SearchResults results={results} />
-    </>
-  );
+    return (
+      <>
+        <SearchInput onSearch={this.getData} />
+        <SearchResults results={results} />
+      </>
+    );
+  }
 }
 
 export default App;
