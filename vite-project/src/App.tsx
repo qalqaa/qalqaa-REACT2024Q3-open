35355c96e93cfa54cd9efbe7c033ef0ebad8778f
qalqa <andreybas04@gmail.com>
2024-07-08 14:10:49 +0300

    refactor: refactored App like FC

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { useEffect, useState } from "react";
 import SearchInput from "./components/SearchInput";
 import SearchResults from "./components/SearchResults";
 import ErrorBoundary from "./components/ErrorBoundry";
@@ -6,70 +6,53 @@ import axios from "axios";
 import "./App.css";
 import Loader from "./components/Loader";
 
-interface IAppState {
-  results: IResult[];
-  error: Error | null;
-  isLoading: boolean;
-}
-
 interface IResult {
   name: string;
   description: string;
 }
 
-interface IProps {}
+const App: React.FC = () => {
+  const [results, setResults] = useState<IResult[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-class App extends Component<IProps, IAppState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      results: [],
-      error: null,
-      isLoading: false,
-    };
-  }
+  useEffect(() => {
+    getData(localStorage.getItem("searchTerm") || "");
+  }, []);
 
-  componentDidMount() {
-    this.getData(localStorage.getItem("searchTerm") || "");
-  }
-
-  getData = async (searchTerm: string) => {
+  const getData = async (searchTerm: string) => {
     const url = searchTerm
       ? `https://swapi.dev/api/people/?search=${searchTerm}`
       : `https://swapi.dev/api/people/`;
-    this.setState({ isLoading: true });
+    setIsLoading(true);
     try {
       const res = await axios.get(url);
-      this.setState({ results: res.data.results });
+      setResults(res.data.results);
     } catch (error) {
       console.error("Error", error);
-      this.setState({ error: error as Error });
+      setError(error as Error);
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
 
-  throwError = () => {
+  const throwError = () => {
     throw new Error("Test error");
   };
 
-  render() {
-    const { results, error, isLoading } = this.state;
-
-    return (
-      <ErrorBoundary>
-        <SearchInput onSearch={this.getData} />
-        <button onClick={this.throwError}>Throw Error</button>
-        {isLoading ? (
-          <Loader />
-        ) : error ? (
-          <p>Error occurred while fetching data.</p>
-        ) : (
-          <SearchResults results={results} />
-        )}
-      </ErrorBoundary>
-    );
-  }
-}
+  return (
+    <ErrorBoundary>
+      <SearchInput onSearch={getData} />
+      <button onClick={throwError}>Throw Error</button>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <p>Error occurred while fetching data.</p>
+      ) : (
+        <SearchResults results={results} />
+      )}
+    </ErrorBoundary>
+  );
+};
 
 export default App;
