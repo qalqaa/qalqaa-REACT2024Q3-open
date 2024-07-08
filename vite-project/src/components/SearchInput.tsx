f00febdd0e288d4432249bc1175b5d9b3b19d56d
qalqa <andreybas04@gmail.com>
2024-07-08 13:49:27 +0300

    refactor: refactored SearchInput like FC

--- vite-project/src/components/SearchInput.tsx
+++ vite-project/src/components/SearchInput.tsx
@@ -1,50 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface ISearchInputProps {
   onSearch: (searchTerm: string) => void;
 }
 
-interface ISearchInputState {
-  searchTerm: string;
-}
+const SearchInput: React.FC<ISearchInputProps> = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
 
-class SearchInput extends React.Component<
-  ISearchInputProps,
-  ISearchInputState
-> {
-  constructor(props: ISearchInputProps) {
-    super(props);
-    this.state = { searchTerm: "" };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     const savedTerm = localStorage.getItem("searchTerm");
     if (savedTerm) {
-      this.setState({ searchTerm: savedTerm });
+      setSearchTerm(savedTerm);
     }
-  }
+  }, []);
 
-  handleSearch = () => {
-    localStorage.setItem("searchTerm", this.state.searchTerm);
-    this.props.onSearch(this.state.searchTerm);
+  const handleSearch = () => {
+    localStorage.setItem("searchTerm", searchTerm);
+    onSearch(searchTerm);
   };
 
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ searchTerm: event.target.value });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
   };
 
-  render() {
-    return (
-      <div>
-        <input
-          type="text"
-          value={this.state.searchTerm}
-          onChange={this.handleChange}
-        />
-        <button onClick={this.handleSearch}>Search</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <input type="text" value={searchTerm} onChange={handleChange} />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
 
 export default SearchInput;
