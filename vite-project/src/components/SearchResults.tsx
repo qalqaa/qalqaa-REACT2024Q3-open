8b2bae02292207ea98595af281e954c5dd559d3e
qalqa <andreybas04@gmail.com>
2024-07-08 13:56:13 +0300

    refactor: refactored SearchResults like FC

--- vite-project/src/components/SearchResults.tsx
+++ vite-project/src/components/SearchResults.tsx
@@ -9,25 +9,21 @@ interface ISearchResultsProps {
   results: IResult[];
 }
 
-class SearchResults extends React.Component<ISearchResultsProps> {
-  render() {
-    const { results } = this.props;
-
-    if (!results || results.length === 0) {
-      return <p>No results found.</p>;
-    }
-
-    return (
-      <div>
-        {results.map((result, index) => (
-          <div key={index}>
-            <h3>{result.name}</h3>
-            <p>{result.description}</p>
-          </div>
-        ))}
-      </div>
-    );
+const SearchResults: React.FC<ISearchResultsProps> = ({ results }) => {
+  if (!results || results.length === 0) {
+    return <p>No results found.</p>;
   }
-}
+
+  return (
+    <div>
+      {results.map((result, index) => (
+        <div key={index}>
+          <h3>{result.name}</h3>
+          <p>{result.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default SearchResults;
