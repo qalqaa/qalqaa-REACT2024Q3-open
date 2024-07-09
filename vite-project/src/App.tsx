acaf33dc53f771f0e550b4b89235b287ec9871d8
qalqa <andreybas04@gmail.com>
2024-07-09 18:15:21 +0300

    fix: small fix

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import MainRouter from "./components/MainRouter";
-import Navbar from "./components/NavBar";
+// import Navbar from "./components/NavBar";
 
 const App: React.FC = () => {
   return (
